691facd5cd37551813c82f0d96d7c31cc3d57549
Riyad Mney <rmneyz@gmail.com>
2023-08-08 22:22:38 +0100

    first commiit

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+
+@Module({
+  imports: [AuthModule, BookmarkModule],
+})
+export class AppModule {}
